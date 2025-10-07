6aa9e1ca18e3e44ad9d5f809ab16792bb004e4ad
Clifford Odige <clifford.odige18@student.qcc.cuny.edu>
2025-10-07 16:05:43 -0400

    complete

---
+++ lab8/lab8_script.js
@@ -0,0 +1,12 @@
+/*
+* Clifford Odige
+* lab 8, events
+* Oct 7, 2025
+*/
+
+console.log("Student's full name")
+let btnpressme = document.querySelector(".btnpressme")
+btnpressme.addEventListener("click", function(){
+  if(btnpressme.textContent == "PRESS ME!"){
+  }
+})
