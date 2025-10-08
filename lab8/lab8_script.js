691713d74695378b6bd17669b1e09e550b9220ee
Clifford Odige <clifford.odige18@student.qcc.cuny.edu>
2025-10-07 22:29:41 -0400

    complte

--- lab8/lab8_script.js
+++ lab8/lab8_script.js
@@ -3,10 +3,16 @@
 * lab 8, events
 * Oct 7, 2025
 */
-
 console.log("Student's full name")
 let btnpressme = document.querySelector(".btnpressme")
-btnpressme.addEventListener("click", function(){
-  if(btnpressme.textContent == "PRESS ME!"){
+btnpressme.addEventListener("click", function(e){
+  if(e.target.textContent == "PRESS ME!"){
+    e.target.textContent = "Key was pressed"
   }
+  else{
+    e.target.textContent = "PRESS ME!"
+  }
+
+  // toggle between class in css
+  e.target.classList.toggle("btnactive")
 })
